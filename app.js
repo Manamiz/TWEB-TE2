a051c35f67d525b7728dd63b0cc99c5c72f44f1f
Thibaud Duchoud <thibduch@hotmail.com>
2016-01-21 14:16:14 +0100

    Update

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+/*
 var gzippo = require('gzippo');
   var express = require('express');
   var app = express();
  
   app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/app"));
+  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
   app.listen(process.env.PORT || 3000); 
+*/
+
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/app'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/app/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
